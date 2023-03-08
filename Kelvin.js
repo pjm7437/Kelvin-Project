988b142b446603ffa8333c6da1cc235e0a0708e7
pjm7437 <127167506+pjm7437@users.noreply.github.com>
2023-03-08 18:17:41 -0500

    Create Kelvin.js

---
+++ Kelvin.js
@@ -0,0 +1,19 @@
+// setting Kelvin as variable for today's forecast
+const kelvin = 293;
+
+// setting Celsius as a function of Kelvin
+var celsius = kelvin - 273;
+
+// fahrenheit as a function of celsius
+var fahrenheit = Math.floor(celsius * (9/5) + 32);
+
+//newton as a function of fahrenheit
+var newton = Math.floor(celsius * (33/100));
+
+
+console.log(fahrenheit);
+
+
+
+console.log(newton);
+
